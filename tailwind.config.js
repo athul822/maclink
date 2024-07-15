863a19a371b7655e6084375c69f4db972d977bdb
athulssiorbit <athulss@iorbit-tech.com>
2024-07-15 14:48:09 +0530

    tailwind + header nav responsive

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      './*.html',
+      './public/**/*.html',
+      './src/**/*.{js,jsx,ts,tsx,vue}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
